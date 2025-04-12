b10820e9bfc915e717c8b062750e7d07adcd1591
beqaperanidze <beqaperanidze28@gmail.com>
2025-04-12 15:17:49 +0400

    initial commit

---
+++ UserManagementApi/Controllers/UsersController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserManagementApi.Data;
+using UserManagementApi.Models;
+
+// Import your Data namespace
+// Import your Models namespace
+
+namespace UserManagementApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] 
+public class UsersController(ApplicationDbContext context) : ControllerBase
+{
+
+
+    [HttpGet]
+    public ActionResult<IEnumerable<User>> GetUsers()
+    {
+        return context.Users.ToList();
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<User> GetUser(int id)
+    {
+        var user = context.Users.Find(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return user;
+    }
+
+    [HttpPost]
+    public ActionResult<User> CreateUser(User user)
+    {
+        context.Users.Add(user);
+        context.SaveChanges(); 
+
+        return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateUser(int id, User user)
+    {
+        if (id != user.Id)
+        {
+            return BadRequest(); 
+        }
+
+        context.Entry(user).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+
+        try
+        {
+            context.SaveChanges();
+        }
+        catch (Microsoft.EntityFrameworkCore.DbUpdateConcurrencyException)
+        {
+            if (!UserExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent(); 
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteUser(int id)
+    {
+        var user = context.Users.Find(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        context.Users.Remove(user);
+        context.SaveChanges();
+
+        return NoContent(); 
+    }
+
+    private bool UserExists(int id)
+    {
+        return context.Users.Any(e => e.Id == id);
+    }
+}
---
+++ UserManagementApi/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagementApi.Models;
+
+public class User
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Username is required")]
+    [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 characters")]
+    public string Username { get; set; }
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email address")]
+    [StringLength(100, ErrorMessage = "Email cannot exceed 100 characters")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required")]
+    [StringLength(255, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters")]
+    public string PasswordHash { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+}
